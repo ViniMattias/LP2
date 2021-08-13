fb8c1afd803fbfdc88a8618cb5bea5eb21b3b661
ViniMattias <88324863+ViniMattias@users.noreply.github.com>
2021-08-13 15:46:08 -0300

    Add files via upload

---
+++ 02-Java2D/Hello2DApp.java
@@ -0,0 +1,36 @@
+  
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Hello2DApp {
+    public static void main (String[] args) {
+        Hello2DFrame frame = new Hello2DFrame();
+    }
+}
+
+class Hello2DFrame extends JFrame {
+    public Hello2DFrame () {
+        this.addWindowListener (
+            new WindowAdapter() {
+                public void windowClosing (WindowEvent e) {
+                    System.exit(0);
+                }
+            }
+        );
+        this.setTitle("Java2D - Hello World!");
+        this.setSize(350, 350);
+        this.setVisible(true);
+    }
+
+    public void paint (Graphics g) {
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setPaint(Color.pink); // 1-Mudei a cor para rosa
+        int w = getWidth();
+        int h = getHeight();
+        g2d.drawLine(0,0, w,h);
+        g2d.drawLine(0,h, w,0);
+        g2d.drawRect(300, 450,300,450); //2- Adicionei um retangulo com estas medidas a minha figura anterior
+    }
+}
